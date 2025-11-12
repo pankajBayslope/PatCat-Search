e5d890960c566592295e9e650ac7fd2931e8fccf
Pankaj9576 <pankajcpr9576@gmail.com>
2025-11-12 17:56:34 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
     setKeywords([])
 
     try {
-      const res = await fetch("http://localhost:8000/search", {
+      const res = await fetch("https://pat-cat-backend-search.vercel.app/search", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ query })
